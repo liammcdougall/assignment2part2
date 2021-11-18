c159db957070746a156708160b90a2c196302d52
Liam <liam.mcdougall1999@gmail.com>
2021-11-17 19:06:25 -0800

    strated the project

--- script.js
+++ script.js
@@ -1,1 +1,68 @@
-/* This file is where all your work should go for Assignment 2.  Please remove this comment. */
+
+function cleanUpIndex() {
+    //removes all of the .contact elements
+    element = document.querySelector(".main")
+    element.innerHTML = ""
+
+}
+
+function createSingleIndex(contact) {
+
+    <p> ${contact[0].name} </p>
+    
+
+
+    
+}
+
+function renderIndex(contactList) {
+    // sub with for of loop    
+
+    for (let i = 0; i < contactList.length; i++) {
+        createSingleIndex(contact)
+      }
+
+}
+
+
+function cleanUPView() {
+    // page3 is the View page, remove the .contactinfo
+    element = document.querySelector(".conatctinfo")
+    element.innerHTML = ""
+}
+
+function renderView(contactList) {
+    
+}
+
+function cleanUpCreate() {
+    //page2, remove the .form
+    element = document.querySelector(".form")
+    element.innerHTML = ""
+    
+}
+
+function renderCreate(contact) {
+    
+}
+
+
+const contactList = [  
+	{ 
+		name: "Oliver Queen", 
+		phone: "778-555-1234", 
+		address: "101 Main St, Star City, USA",    
+		email: "greenarrow@watchtower.com",  
+	},  
+
+	{    
+		name: "Jessica Cruz",    
+		phone: "123-555-5555",    
+		address: "Portland Oregon",    
+		email: "greenlantern@watchtower.com",  
+	}
+]
+
+
+
+
